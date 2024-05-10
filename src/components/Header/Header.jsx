b573bff43e299622ecfb5b49d96635ff302c0f41
Sebbybobbler <seb_davies@hotmail.co.uk>
2024-05-10 15:39:40 +0100

    added images, tweaked circle somewhat

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,17 @@
 import styles from "./Header.module.css";
-
+import SiteNav from "../SiteNav/SiteNav";
 export default function Header() {
     return (
         <>
             <div className={styles.wrapper}>
                 <div className={styles.circle}>
                 <div className={styles.circleContent}>
+                <div className={styles.circleContentHeading}>
+                <img className={styles.circleContentHeadingLeftImage} src={"./images/ducklogo.png"}/>
                 <h1>Our Animals</h1>
+                <img src={"./images/ducklogo.png"}/>
+                </div>
+                <SiteNav/>
                 <h4>Which is your favourite?</h4>
                 </div>
                 </div>
