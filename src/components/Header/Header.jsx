6e783f204f2cd5ad84293fc7f250dc735be0cae4
Sebbybobbler <seb_davies@hotmail.co.uk>
2024-05-10 14:57:45 +0100

    can't believe it took me an hour to make a circle and have it partially hidden (:

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,16 @@
 import styles from "./Header.module.css";
 
-export default function Header(){
-    return <><p className={styles.wrapper}>I am the header</p></>
+export default function Header() {
+    return (
+        <>
+            <div className={styles.wrapper}>
+                <div className={styles.circle}>
+                <div className={styles.circleContent}>
+                <h1>Our Animals</h1>
+                <h4>Which is your favourite?</h4>
+                </div>
+                </div>
+            </div>
+        </>
+    );
 }
